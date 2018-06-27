ff4e0f03aa058bacdadf60c69675f8ae100b6f64
Johnathan Lee <jfl747076@gmail.com>
2018-06-27 16:36:29 -0500

    Moving to its own repository

---
+++ EnumMap.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+// Use this to mark an enum for mapping
+#define STRINGIFY __attribute__((annotate("Stringify")))
+
+// This is the function you call to convert from a string to your enum. It returns T(UINT_MAX) if it fails 
+template <typename T>
+T from_string(const std::string& in);
+
+// For enum->string, we use a specialization of to_string, so just call that.
---
+++ enummap.py
@@ -0,0 +1,50 @@
+#!/bin/env python3
+
+import os, xml.etree.ElementTree, sys
+
+# TODO: Make cross-platform, since not all have /tmp
+tmpDir = '/tmp/' 
+
+if len(sys.argv) < 3:
+    print("Usage: ./enummap.py <input> <output> [<include dirs>...]")
+    exit(-1)
+
+
+fileName = sys.argv[1]
+astFileName = tmpDir + os.path.basename(fileName) + ".xml"
+
+includeDirs = ""
+for i in range(3, len(sys.argv)):
+    includeDirs += " -cxx-isystem " + sys.argv[i]
+
+castXmlArgs = includeDirs + " -std=c++17 --castxml-output=1 -o " + astFileName
+
+os.system("castxml " + fileName + castXmlArgs)
+
+ast = xml.etree.ElementTree.parse(astFileName).getroot()
+
+mapFile = open(sys.argv[2], 'w')
+mapFile.write("#pragma once\n#include <unordered_map>\n#include <" + os.path.basename(fileName) + ">\n")
+mapFile.write("// Generated from " + fileName + "\n")
+
+for enum in ast.findall('Enumeration'):
+    if enum.get("attributes") != "annotate(Stringify)":
+        continue
+    name = enum.get("name")
+    mapFile.write("static const std::unordered_map<" + name + ", std::string> " + name + "Mappings = {")
+    for val in enum:
+        value = val.get("name")
+        mapFile.write("{" + name + "::" + value + ", \"" + value + "\"},\n")
+    mapFile.write('};\n')
+    mapFile.write("""
+    inline const std::string& to_string(const """ + name + """ from) { return """ + name + """Mappings.at(from); } 
+   template <>                                                                                 
+   inline """ + name +""" from_string<"""+ name + """>(const std::string& in) {                              
+      for (const auto& pairing : """ + name + """Mappings)                                                 
+         if (pairing.second == in)                                                             
+            return pairing.first;
+    return """ + name + """(UINTMAX_MAX);                                                         
+   }""")
+
+os.system("rm " + astFileName)
+
